1fd6b979269c73026bade44a14bf44c47a11f2ac
gergokutu <gergo.kutu@gmail.com>
2020-11-24 16:39:50 +0100

    Implement behaviour > methods on object

--- oop_1.js
+++ oop_1.js
@@ -1,7 +1,11 @@
 let dog = {
   'name': 'Snuffie',
-  numLegs: 4
+  numLegs: 4,
+  sayName: function () { return "The name of this dog is " + dog['name'] + "."; },
+  sayLegs() { return `This dog has ${dog.numLegs} legs.`; }
 };
 
 console.log('Dog name:', dog.name);
 console.log('Number of legs:', dog['numLegs']);
+console.log('Say name >', dog.sayName());
+console.log('Say name >', dog['sayLegs']());
