c149afa282accf3cb416448037ab17bc8f4af6a4
gergokutu <gergo.kutu@gmail.com>
2020-11-24 16:59:03 +0100

    Use arrow synthax, this

--- oop_1.js
+++ oop_1.js
@@ -1,11 +1,15 @@
 let dog = {
   'name': 'Snuffie',
   numLegs: 4,
-  sayName: function () { return "The name of this dog is " + dog['name'] + "."; },
-  sayLegs() { return `This dog has ${dog.numLegs} legs.`; }
+  sayName: function () { return 'The name of this dog is ' + dog['name'] + '.'; },
+  normalThis: function () { return 'Not in arrow function > ' + this.name },
+  arrowThis: () => { return 'Be careful > ' + this.name },
+  sayLegs() { return `This dog has ${dog.numLegs} legs.`; },
 };
 
 console.log('Dog name:', dog.name);
 console.log('Number of legs:', dog['numLegs']);
 console.log('Say name >', dog.sayName());
 console.log('Say name >', dog['sayLegs']());
+console.log('Normal this:', dog.normalThis());
+console.log('Arrow this:', dog.arrowThis());
